dce253ca7c1efa7646876781a2e368a2d7832dd8
Artur Papoyan <papoyan.340@gmail.com>
2023-03-03 17:17:20 +0400

    add useEfect + useRef

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-import {Header, Categories} from "./Components";
+import { Header, Categories, SortPopup,  } from "./Components";
+
 
 function App() {
+
+  
+
+
   return (
     <div className="wrapper">
       <div className="wrapper">
@@ -10,32 +15,17 @@ function App() {
         <div className="content">
           <div className="container">
             <div className="content__top">
-              <Categories items = {['Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}/>
-              <div className="sort">
-                <div className="sort__label">
-                  <svg
-                    width="10"
-                    height="6"
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                      fill="#2C2C2C"
-                    />
-                  </svg>
-                  <b>Сортировка по:</b>
-                  <span>популярности</span>
-                </div>
-                <div className="sort__popup">
-                  <ul>
-                    <li className="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
-                  </ul>
-                </div>
-              </div>
+              <Categories
+                onClick ={(name) => console.log(name)}
+                items={[
+                  "Мясные",
+                  "Вегетарианская",
+                  "Гриль",
+                  "Острые",
+                  "Закрытые",
+                ]}
+              />
+              <SortPopup/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
@@ -57,7 +47,7 @@ function App() {
                     <li>40 см.</li>
                   </ul>
                 </div>
-                <div className="pizza-block__bottom">
+                <div  className="pizza-block__bottom">
                   <div className="pizza-block__price">от 395 ₽</div>
                   <div className="button button--outline button--add">
                     <svg
--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Categories({item}) {
+function Categories({items, onClick}) {
+
+  const [activeItem, setActiveItem] = useState('')  
+
+  // const state = React.useState(null);
+  // const activeItem = state[0];
+  // const setActiveItem = state[1];
+  // useState  ashxatanq@ takic
+
   return (
     <div className="categories">
         <ul>
-          <li className="active">Все</li>
-          <li>Мясные</li>
-          {/* {
-            item.map(el => <li>el</li>)
-          } */}
-          {/* <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li> */}
+          <li>Все</li>
+         {
+            items?.map((el, idx) => <li  onClick={() => setActiveItem(prev => prev = idx)} key={`${el}_ ${idx}`}
+                                        className ={activeItem === idx ? 'active' : ''}
+                                    >{el}</li>)
+         }
+          
         </ul>
     </div>
   )
 }
+
+// class Categories extends React.Component {
+
+//   state = {
+//     activeItems: null
+//   }
+
+//   onselectItems =(idx) => {
+//     this.setState({
+//       activeItems: idx
+//     })
+//     // this.forceUpdate() kanchum e rerender
+//   }
+
+//   render(){
+
+//     const {items, onClick} = this.props
+
+//     return (
+//       <div className="categories">
+//           <ul>
+//             <li>Все</li>
+//            {
+//               items.map((el, idx) => <li  onClick={() => this.onselectItems(idx)} key={`${el}_ ${idx}`}
+//                                           className={this.state.activeItems === idx ? 'active' : ''}
+//                                         >{el}</li>)
+//            }
+            
+//           </ul>
+//       </div>
+//     )
+//   }
+// }
 
 export default Categories
