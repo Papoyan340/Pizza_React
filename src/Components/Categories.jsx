498241201d90752d41e8c366d6191aa0cf213e4d
Artur Papoyan <papoyan.340@gmail.com>
2023-03-06 01:29:21 +0400

    add new component pizzaBlock, home,  add router PropTupesf

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,27 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-function Categories({items, onClick}) {
-
-  const [activeItem, setActiveItem] = useState('')  
+function Categories({ items, onClick }) {
+  const [activeItem, setActiveItem] = useState('');
 
   // const state = React.useState(null);
   // const activeItem = state[0];
   // const setActiveItem = state[1];
   // useState  ashxatanq@ takic
 
+  const onSelectItem = (idx) => {
+    setActiveItem((prev) => (prev = idx));
+  };
+
   return (
     <div className="categories">
-        <ul>
-          <li>Все</li>
-         {
-            items?.map((el, idx) => <li  onClick={() => setActiveItem(prev => prev = idx)} key={`${el}_ ${idx}`}
-                                        className ={activeItem === idx ? 'active' : ''}
-                                    >{el}</li>)
-         }
-          
-        </ul>
+      <ul>
+        <li>Все</li>
+        {items?.map((el, idx) => (
+          <li
+            onClick={() => onSelectItem(idx)}
+            key={`${el}_ ${idx}`}
+            className={activeItem === idx ? 'active' : ''}>
+            {el}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 // class Categories extends React.Component {
@@ -50,11 +55,11 @@ function Categories({items, onClick}) {
 //                                           className={this.state.activeItems === idx ? 'active' : ''}
 //                                         >{el}</li>)
 //            }
-            
+
 //           </ul>
 //       </div>
 //     )
 //   }
 // }
 
-export default Categories
+export default Categories;
